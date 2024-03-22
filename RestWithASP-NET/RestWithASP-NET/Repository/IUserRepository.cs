f1474582bd21a15387c02e509245bb4499453a28
marliseborba <borba.marlise@gmail.com>
2024-03-22 00:38:37 -0300

    Creating UserRepository

---
+++ RestWithASP-NET/RestWithASP-NET/Repository/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using RestWithASP_NET.Data.VO;
+using RestWithASP_NET.Model;
+
+namespace RestWithASP_NET.Repository
+{
+    public interface IUserRepository
+    {
+        User? ValidateCredentials(UserVO user);
+    }
+}
---
+++ RestWithASP-NET/RestWithASP-NET/Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using RestWithASP_NET.Data.VO;
+using RestWithASP_NET.Model;
+using RestWithASP_NET.Model.Context;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RestWithASP_NET.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySQLContext _context;
+
+        public UserRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public User? ValidateCredentials(UserVO user)
+        {
+            var pass = ComputeHash(user.Password, SHA256.Create());
+            return _context.Users.FirstOrDefault(u => (u.UserName == user.Username) && (u.Password == pass));
+        }
+
+        private string ComputeHash(string input, SHA256 algorithm)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashedBytes);
+        }
+    }
+}
