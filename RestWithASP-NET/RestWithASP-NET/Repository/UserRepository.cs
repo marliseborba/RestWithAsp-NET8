5bba284fd6f101675fdacf907fca1c0ed7d35c44
marliseborba <borba.marlise@gmail.com>
2024-03-22 01:13:15 -0300

    Create RefreshUserInfo method

--- RestWithASP-NET/RestWithASP-NET/Repository/UserRepository.cs
+++ RestWithASP-NET/RestWithASP-NET/Repository/UserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using RestWithASP_NET.Data.VO;
 using RestWithASP_NET.Model;
 using RestWithASP_NET.Model.Context;
+using System.Data;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -19,6 +20,28 @@ namespace RestWithASP_NET.Repository
         {
             var pass = ComputeHash(user.Password, SHA256.Create());
             return _context.Users.FirstOrDefault(u => (u.UserName == user.Username) && (u.Password == pass));
+        }
+
+        public User RefreshUserInfo(User user)
+        {
+            if (!_context.Users.Any(p => p.Id.Equals(user.Id)))
+                return null;
+
+            var result = _context.Users.SingleOrDefault(p => p.Id.Equals(user.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(user);
+                    _context.SaveChanges();
+                    return result;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return result;
         }
 
         private string ComputeHash(string input, SHA256 algorithm)
